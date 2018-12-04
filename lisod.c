a1b1d541dd7e5687a375e02ad743e9baba3a55ed
lzx <1942374438@qq.com>
2018-12-04 16:32:17 +0800

    it now can daemonize

--- lisod.c
+++ lisod.c
@@ -2,28 +2,42 @@
 #include "utilities/common.h"
 #include "handler/handle_client.h"
 #include "lisod.h"
+#include <fcntl.h>
+#include <signal.h>
+#define RUNNING_DIR "/home/lzx/computer_network/liso_server/"
+//#define DEBUG_VERBOSE 1
 //char log_dir[BUF_SIZE] = "/home/lzx/computer_network/liso_server/log/";
-//fok
+//fo
+
 int logging_fd;
 char *logging_path;
 char *WWW_FOLDER;
+char *lock_file;
 client_pool pool;
-#define USAGE "liso server usage:\n%s http port logfile wwwfolder\n "
+#define USAGE "liso server usage:\n%s http port https port logfile lockfile wwwfolder private key file certificate file\n "
+
 // git test
 int main(int argc, char *argv[]){
     int sockfd;
     int client_sockfd;
 
-    if(argc < 4){
+    if(argc < 7){
         fprintf(stderr, USAGE, argv[0]);
         exit(-1);
     }
+
+
     int port = atoi(argv[1]);
     logging_path = argv[2];
-    init_logging();
-    WWW_FOLDER = argv[3];
+    //init_logging();x
+    lock_file = argv[3];
+    WWW_FOLDER = argv[4];
+    #ifdef DEBUG_VERBOSE
+         daemonize();
+    #endif
     create_dictionary(WWW_FOLDER, S_IRWXU|S_IRWXG | S_IRWXO|S_IROTH|S_IXOTH);
     //then create socket, bind, listen,
+    init_logging();
     sockfd = create_server_sock(port);
     init_pool(sockfd);
     log_info("HTTP PORT: %d\n", port);
@@ -55,4 +69,44 @@ int main(int argc, char *argv[]){
         }
     }
 }
+int daemonize(){
+    int i, lfp, pid = fork();
+    char str[256] = {0};
+    if (pid < 0) exit(-1);
+    if(pid > 0) exit(0);
+    setsid();
 
+    for(i = getdtablesize(); i >= 0; i--)   close(i);
+
+    i = open("/dev/null", O_RDWR);
+    dup(i);
+    dup(i);
+    umask(027);
+    // chdir(RUNNING_DIR);
+    lfp = open(lock_file, O_RDWR|O_CREAT, 0640);
+
+    if(lfp < 0)   exit(-1);
+    if(lockf(lfp, F_TLOCK, 0) < 0)   exit(0); /*can not lock*/
+
+    sprintf(str, "%d\n", getpid());
+    write(lfp, str, strlen(str));
+    //log_info("hello world\n");
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+    signal(SIGTERM, SIG_IGN);
+    return 0;
+}
+
+/*
+ *internal signal handler
+ */
+void signal_handler(int sig){
+    switch(sig){
+    case SIGHUP:
+        break;
+    case SIGTERM:
+        break;
+    default:
+        break;
+    }
+}
